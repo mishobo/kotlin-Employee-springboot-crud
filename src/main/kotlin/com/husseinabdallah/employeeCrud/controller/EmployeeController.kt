1f9cb4b9d75747cc24521848740bf346b9b1f3c7
mishobo <60912349+mishobo@users.noreply.github.com>
2022-08-30 05:48:00 +0300

    adding EmployeeController

---
+++ src/main/kotlin/com/husseinabdallah/employeeCrud/controller/EmployeeController.kt
@@ -0,0 +1,28 @@
+package com.husseinabdallah.employeeCrud.controller
+
+import com.husseinabdallah.employeeCrud.model.Employee
+import com.husseinabdallah.employeeCrud.service.EmployeeService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class EmployeeController(private val employeeService: EmployeeService) {
+
+    @GetMapping("/employees")
+    fun getAllEmployees(): List<Employee> = employeeService.getAllEmployees()
+
+    @GetMapping("/employees/{id}")
+    fun getEmployeesById(@PathVariable("id") employeeId: Long): Employee =
+        employeeService.getEmployeesById(employeeId)
+
+    @PostMapping("/employees")
+    fun createEmployee(@RequestBody payload: Employee): Employee = employeeService.createEmployee(payload)
+
+    @PutMapping("/employees/{id}")
+    fun updateEmployeeById(@PathVariable("id") employeeId: Long, @RequestBody payload: Employee): Employee =
+        employeeService.updateEmployeeById(employeeId, payload)
+
+    @DeleteMapping("/employees/{id}")
+    fun deleteEmployeesById(@PathVariable("id") employeeId: Long): Unit =
+        employeeService.deleteEmployeesById(employeeId)
+
+}
